58127d1af5d1f9011217b248c6794cb0d3d6457e
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-08-12 17:29:39 -0700

    update 48

--- get_sepsis_score.py
+++ get_sepsis_score.py
@@ -64,21 +64,11 @@ def get_sepsis_score(data, model, target_labels=None, adversary=False):
     if adversary:
         explorer = process_config_file(cf, model, feature_extractor=feature_extractor, input_processor_list=[])
         explorer.scoring_function = mse
-        allPatients_benign = data#.reshape(backcast_length*nv)#data.reshape(-1, backcast_length*nv)
+        allPatients_benign = data
         explore_params = [allPatients_benign, backcast_length, nv]
         allPatients_adversarial = explorer.explore(explore_params)
         allPatients_adversarial = np.array(allPatients_adversarial).reshape(backcast_length, nv)
-        print('allPatients_benign')
-        print(allPatients_benign)
-        print(allPatients_benign.shape)
-        print(type(allPatients_benign))
-        print('---------------------------------------------------')
-        print('allPatients_adversarial')
-        print(allPatients_adversarial)
-        print(allPatients_adversarial.shape)
-        print(type(allPatients_adversarial))
-        print('---------------------------------------------------')
-        exit(1)
+        data = allPatients_adversarial
     # Nawawy's end
     norm = [2.800e+02, 1.000e+02, 5.000e+01, 3.000e+02, 3.000e+02, 3.000e+02, 1.000e+02,
             1.000e+02, 1.000e+02, 5.500e+01, 4.000e+03, 7.930e+00, 1.000e+02, 1.000e+02,
