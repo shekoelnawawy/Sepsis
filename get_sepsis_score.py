64075ab8c119c4aa494df2914de6be27b66eb703
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-08-13 17:24:43 -0700

    update 71

--- get_sepsis_score.py
+++ get_sepsis_score.py
@@ -31,14 +31,18 @@ def mse(output, target):
 # Nawawy's end
 
 class Model(nn.Module):
-    def __init__(self,input_size,hidden_size,hidden_depth):
+    # Nawawy's start
+    def __init__(self,input_size,hidden_size,hidden_depth, device=None):
+    # Nawawy's end
         super(Model, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.hidden_depth = hidden_depth
-        self.lstm = nn.LSTM(input_size, self.hidden_size, self.hidden_depth, bidirectional=False, dropout=0)
-        self.fc1 = nn.Linear(1*self.hidden_size, 2)
-
+        # Nawawy's start
+        self.device = device
+        self.lstm = nn.LSTM(input_size, self.hidden_size, self.hidden_depth, bidirectional=False, dropout=0, device = self.device)
+        self.fc1 = nn.Linear(1*self.hidden_size, 2, device = self.device)
+        # Nawawy's end
     def forward(self, x):
         h0 = torch.zeros(self.hidden_depth * 1, 1, self.hidden_size).to(x.device)
         c0 = torch.zeros(self.hidden_depth * 1, 1, self.hidden_size).to(x.device)
@@ -81,7 +85,7 @@ def get_sepsis_score(data, model, target_labels=None, adversary=False):
             7.170e+01, 3.200e+01, 2.500e+02, 4.400e+02, 1.760e+03, 2.322e+03, 1.000e+02,
             1.000e+00, 1.000e+00, 1.000e+00, 2.399e+01, 3.360e+02]
     data = data/norm
-    data = torch.Tensor(data).float().to(device)
+    data = torch.Tensor(data).float()#.to(device)
     threshold = 0.10
     _, probs = model(data)
     probs = probs[:, 1]
@@ -90,7 +94,10 @@ def get_sepsis_score(data, model, target_labels=None, adversary=False):
 
 def load_sepsis_model():
     modelpth = './model_1561740354_cv_0_16_0.09277561709115473'
-    model = Model(40,100,2)
+    # Nawawy's start
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model = Model(40,100,2, device)
+    # Nawawy's end
     model.load_state_dict(torch.load(modelpth))
     model.eval()
     return model
