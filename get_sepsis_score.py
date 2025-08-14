8ae491a2b9fd2ece4c19bd4be83026ecafe61ff0
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-08-13 17:28:31 -0700

    update 74

--- get_sepsis_score.py
+++ get_sepsis_score.py
@@ -95,6 +95,7 @@ def get_sepsis_score(data, model, target_labels=None, adversary=False):
     # Nawawy's start
     return probs.cpu().data.numpy()[0], argmax.cpu().data.numpy()[0]
     # Nawawy's end
+
 def load_sepsis_model():
     modelpth = './model_1561740354_cv_0_16_0.09277561709115473'
     # Nawawy's start
